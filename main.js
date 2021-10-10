40abc9767c7bf5ad67133c562062fb8a29d37d8c
sandra <mjpfelicia@gmail.com>
2021-10-10 17:53:58 -0300

    melhorias no css

---
+++ main.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {HTMLElement} x 
+ */
+
+function menuHamburger(btnMenu) {
+    const linkHeader = document.querySelector(".link-header");
+    btnMenu.classList.toggle("change");
+    linkHeader.style.display = "block";
+    console.log(x);
+}
